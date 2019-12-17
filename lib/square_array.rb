3d59ae2636975bc7a278010a728242fcc8667955
David Van Fleet <dvfleet413@gmail.com>
2019-12-17 16:42:11 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,9 @@
 def square_array(array)
   counter = 0
+  new_array = []
   while (counter < array.length) do
-    array[counter] = array[counter]**2
+    new_array[counter] = array[counter]**2
     counter += 1
   end
+  new_array
 end
